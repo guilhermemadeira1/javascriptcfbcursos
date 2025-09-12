9163cf17d577f0e766343fa96a24364f67adad95
guilhermemadeira1 <161243338+guilhermemadeira1@users.noreply.github.com>
2025-09-12 15:03:24 -0300

    aulas 070 a 074

--- aula002_use-strict/use-strict.js
+++ aula002_use-strict/use-strict.js
@@ -2,7 +2,7 @@
 
 /*Inserir "use strict" no início de um arquivo JavaScript ou no início de um bloco permite escrever códigos em modo estrito.
 
-O modo estrito permite escrver códigos menos propensos a erros implícitos que são contornados pela linguagem e causam saídas inesperadas. Os erros antes contornados pela linguagem, passam a lançar exceções.
+O modo estrito permite escrever códigos menos propensos a erros implícitos que são contornados pela linguagem e causam saídas inesperadas. Os erros antes contornados pela linguagem, passam a lançar exceções.
 
 O modo estrito não permite, por exemplo, atribuir valor a uma variável não declarada. Em modo não estrito, o JavaScript declara implicitamente uma variável não declarada ao receber um valor.
 
